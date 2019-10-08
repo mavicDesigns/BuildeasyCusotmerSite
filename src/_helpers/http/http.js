c511a51f0b5f74440da70d4c7c9ae2ae764b5ef4
unknown <Olurin anjola>
2019-10-08 16:40:13 +0100

    Change of Base Url

--- src/_helpers/http/http.js
+++ src/_helpers/http/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'http://localhost/buildeasyApi/public/',
+    baseURL: process.env.VUE_APP_baseUrl,
     headers:{
         Authorization: 'Bearer {token}'
     }
