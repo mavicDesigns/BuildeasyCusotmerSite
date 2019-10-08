02796baf3da6574dccd873b3d35fb638bafe0e4b
unknown <Olurin anjola>
2019-10-08 17:57:20 +0100

    First Commit

--- src/_helpers/http/http.js
+++ src/_helpers/http/http.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export const http = axios.create({
     baseURL: 'https://secure-forest-32865.herokuapp.com/',
+    withCredentials: true,
     headers:{
-        Authorization: 'Bearer {token}'
+        Authorization: 'Bearer {token}',
+
     }
 });
